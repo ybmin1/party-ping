008dca4f9cf2358afe4988678c4cf561472c565f
ybmin1 <yebin.min1@gmail.com>
2026-06-12 06:32:31 +0900

    ui: add section 2 - map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,3 +1,27 @@
 export default function Map() {
-  return <div>Map</div>;
+  return (
+    <section className="bg-background flex justify-center">
+      <div className="w-full min-w-[360px] max-w-[430px] h-screen flex justify-center items-end">
+        <div
+          className="w-full h-[290px] flex items-end"
+          style={{
+            backgroundImage: "url('/images/circle-bg.png')",
+          }}
+        >
+          <div
+            className="w-full h-[250px] flex justify-center items-center "
+            style={{
+              backgroundImage: "url('/images/circle.png')",
+            }}
+          >
+            <div className="text-center font-lacquer text-primary-text text-xl space-y-4">
+              <div>Time</div>
+              <div>Parking</div>
+              <div>Get Directions</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
