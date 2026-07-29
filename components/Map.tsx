ab99192aea92f566cb0cd3c34d4780739f373d0d
ybmin1 <yebin.min1@gmail.com>
2026-07-30 06:51:43 +0900

    feat: add GoogleMap component

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,23 +1,25 @@
+import GoogleMap from "./GoogleMap";
+
 export default function Map() {
   return (
     <section className="bg-cream flex justify-center">
-      <div className="w-full min-w-[360px] max-w-[430px] h-screen flex justify-center items-end">
+      <div className="w-full min-w-[360px] max-w-[430px] h-screen flex flex-col justify-center items-center relative">
+        <GoogleMap />
         <div
-          className="w-full h-[290px] flex items-end"
+          className="w-full h-[250px] flex items-end absolute bottom-0 left-0"
           style={{
             backgroundImage: "url('/images/circle-bg.png')",
           }}
         >
           <div
-            className="w-full h-[250px] flex justify-center items-center "
+            className="w-full h-[210px] flex justify-center items-center "
             style={{
               backgroundImage: "url('/images/circle.png')",
             }}
           >
-            <div className="text-center font-lacquer text-darkgrey text-xl space-y-4">
-              <div>Time</div>
+            <div className="text-center font-lacquer text-darkgrey text-xl space-y-2">
               <div>Parking</div>
-              <div>Get Directions</div>
+              <div className="text-base">🚗 Free on-site parking available</div>
             </div>
           </div>
         </div>
